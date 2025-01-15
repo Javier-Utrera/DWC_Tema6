dcf123b9e2bd3460e9bc9fb51bebef155e1b577b
Francisco Javier <francisco.rodriguez.garcia.al@iespoligonosur.org>
2025-01-15 19:07:55 +0100

    Comenzando ejercio2

---
+++ Formulario/script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", inicializar, false);
+
+function inicializar() {
+    crearformulario()
+}
+
+function crearformulario(){
+    var formulario;
+    var label;
+    var input;
+
+   var contenedor= document.getElementById("contenedor")
+
+   formulario=document.createElement("from")
+}
+
+
+
